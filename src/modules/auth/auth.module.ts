839fd3c75426f5abc41667f5ea5ef27bdcc0effa
Radsonb <radsonbrendon02@gmail.com>
2026-06-03 21:43:24 -0300

    feat: create login service

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -2,9 +2,25 @@ import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/shared/prisma/prisma.module';
+import { PassportModule } from '@nestjs/passport';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'jwt' }),
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (config: ConfigService) => ({
+        secret: config.getOrThrow<string>('JWT_ACCESS_SECRET'),
+        signOptions: {
+          expiresIn: config.getOrThrow<string>('JWT_ACCESS_EXPIRES_IN') as `${number}${'s' | 'm' | 'h' | 'd'}`,
+        },
+      }),
+    }),
+    PrismaModule
+  ],
   controllers: [AuthController],
   providers: [AuthService],
 })
